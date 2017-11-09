3de227f981e1ddb16a623fcaba74f64685209024
Haley Ward <hayevans18@gmail.com>
2017-11-09 14:35:28 -0600

    Added basic folders

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('video').mediaelementplayer({
+      features: ['playpause', 'progress', 'volume', 'fullscreen'],
+      startLanguage: 'en',
+    });
+
+
+const video = document.querySelector("video");
+const span = document.querySelectorAll("span");
+
+
+
+for (let i = 0; i < span.length; i += 1) {
+    span[i].addEventListener("click", () => {
+        span[i].style.backgroundColor= "lightyellow";
+    });
+}
