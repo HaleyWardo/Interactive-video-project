f3007f895d8210e1648506f127841c707ed4fafd
Haley Ward <hayevans18@gmail.com>
2017-11-09 22:15:05 -0600

    Added javascript events
    Added javascript events. One for when user clicks on a span the video will start playing at time selected. Another one for when the video plays current span section is highlighted.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-
-const video = document.querySelector("video");
-const span = document.querySelectorAll("span");
-
-
-
-for (let i = 0; i < span.length; i += 1) {
-    span[i].addEventListener("click", () => {
-        span[i].style.backgroundColor= "lightyellow";
+$('video').mediaelementplayer({
+      features: ['playpause', 'progress', 'volume', 'fullscreen'],
     });
-}
+
+
+const video = document.querySelector('video');
+const allSpans = document.querySelectorAll('.transcript span')
+
+
+//As video plays - current section highlights
+video.addEventListener('timeupdate', () => {
+    const currentTime = video.getCurrentTime();
+
+    for (let i = 0; i < allSpans.length; i++) {    //loops through each span
+        const span = allSpans[i];
+        const start = parseFloat(span.dataset.start);  //takes the string and returns it as a decimal
+        const end = parseFloat(span.dataset.end);
+
+        if (currentTime > start && currentTime < end) { 
+            span.style.color= '#fff';    //highlighted color
+        } else {
+            span.style.color = '#3C414E';  //default color
+        }
+    }
+});
+
+
+//On click, video jumps to target section
+for  (let i = 0; i < allSpans.length; i++) {
+    const span = allSpans[i];
+    const start = parseFloat(span.dataset.start);
+    const end = parseFloat(span.dataset.end);
+
+    span.addEventListener('click', () => {
+        if (span) {
+            video.setCurrentTime(start);
+            video.play();
+        }
+    }); 
+};
